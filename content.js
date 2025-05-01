a84173ad3ba4f5fe9e7855cefb66cc04389af6ee
galbimandu <galbimandu1126@gmail.com>
2025-05-01 21:36:41 +0900

    chore : 5초 단위로 다시 제동 걸도록 함

--- content.js
+++ content.js
@@ -1,32 +1,36 @@
-(function() {
+(function () {
   // shorts-container 스크롤 방지
-  const style = document.createElement('style');
+  const style = document.createElement("style");
   style.textContent = `
     #shorts-container {
       overflow: hidden !important;
     }
   `;
   document.head.appendChild(style);
-  
+
   // navigation-container 클래스를 가진 요소 삭제
   function removeNavigationContainer() {
-    const navContainers = document.getElementsByClassName('navigation-container');
-    while(navContainers.length > 0) {
+    const navContainers = document.getElementsByClassName(
+      "navigation-container"
+    );
+    while (navContainers.length > 0) {
       navContainers[0].remove();
     }
   }
-  
+
   // 초기 실행
   removeNavigationContainer();
-  
+
   // 동적으로 추가되는 요소를 위한 MutationObserver
   const observer = new MutationObserver(() => {
     removeNavigationContainer();
   });
-  
+
   // body의 변화 감시
   observer.observe(document.body, {
     childList: true,
-    subtree: true
+    subtree: true,
   });
+
+  setInterval(removeNavigationContainer, 500);
 })();
